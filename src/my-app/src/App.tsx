6f2514de0d6dbc472f6a0bc1170bc94206792d27
Paulo Ricardo Mesquita <87778216+PaPaPaulitos@users.noreply.github.com>
2023-09-19 18:59:59 -0300

    Version 0.6

--- src/my-app/src/App.tsx
+++ src/my-app/src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GlobalStyle from './Styles/global';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { Outlet } from 'react-router-dom';
-
 import Header from './Components/Header/Header';
 
 
@@ -13,6 +12,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Outlet/>
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/my-app/src/Styles/global.tsx
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box
+  }
+
+  body{
+    font-family: 'Roboto', sans-serif;
+    background-color: #E6E6E6;
+  }
+
+
+  // Adicione quais mais regras desejar!
+`;
