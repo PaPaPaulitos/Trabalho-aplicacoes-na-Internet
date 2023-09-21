41401447616110e9a0357ffeb12346cff82752a3
Paulo Ricardo Mesquita <87778216+PaPaPaulitos@users.noreply.github.com>
2023-09-21 11:54:59 -0300

    Component FormCreatEvent

--- src/my-app/src/Components/FormCreateEvent/FormCreateEvent.tsx
+++ src/my-app/src/Components/FormCreateEvent/FormCreateEvent.tsx
@@ -24,9 +24,9 @@ function FromCreateEvent() {
                 <label htmlFor="createEvent" className='label_form'>Evento é privado?</label>
                 <br/>
                 <div className='radio'>
-                    <input type="radio" name="evento_privado" id="evento_privado" value={"sim"} className='radio_fom' />
+                    <input type="radio" name="evento_privado" id="evento_privado" value={"sim"} />
                     <label htmlFor="createEvent" className='label_form'>Sim</label>
-                    <input type="radio" name="evento_privado" id="evento_privado" value={"nao"} className='radio_fom' />
+                    <input type="radio" name="evento_privado" id="evento_privado" value={"nao"} />
                     <label htmlFor="createEvent" className='label_form'>Não</label>
                 <br/>
                 </div>
@@ -34,9 +34,8 @@ function FromCreateEvent() {
                 <br/>
                 <textarea name="descricao_evento" id="descricao_evento" cols={30} rows={10} className='area_form'></textarea>
                 <br/>
-                <label htmlFor="createEvent" className='label_form'>Imagem do Evento:</label>
-                <br/>
-                <input type="file" id="image" name="image" placeholder='Escolher Imagem' className="fbtn btn-primary"></input>
+                    <label htmlFor="arquivo" className='btn btn-primary'>Enviar imagem do evento</label>
+                    <input type="file" name="arquivo" id="arquivo"/>
                 <br/>
                 <br/>
                 <input type="submit" className="btn btn-primary" value="Criar Evento"></input>
