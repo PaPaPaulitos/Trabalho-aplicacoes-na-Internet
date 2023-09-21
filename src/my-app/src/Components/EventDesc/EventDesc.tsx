f1fd52cdbce6bd066cb7b9e52c72cb2f0bdfb65d
Paulo Ricardo Mesquita <87778216+PaPaPaulitos@users.noreply.github.com>
2023-09-21 15:07:48 -0300

    Update EventDesc.tsx

--- src/my-app/src/Components/EventDesc/EventDesc.tsx
+++ src/my-app/src/Components/EventDesc/EventDesc.tsx
@@ -2,23 +2,31 @@ import React from 'react';
 import './EventDesc.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
-import * as Icon from 'react-bootstrap-icons';
+import { ChatSquareTextFill,
+        GeoAltFill,
+        PeopleFill,
+        Calendar2CheckFill
+} from 'react-bootstrap-icons';
 
 function EventDesc() {
     return (
-        <div>
-            <p>AAAAAAA</p>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+
+                </div>
+                <div className="col">
+                    <h1>EVENTO</h1>
+                    <p className="event-city"><GeoAltFill/>CIDADE DO EVENTO</p>
+                    <p className="event-owner">DONO DO EVENTO</p>
+                    <p className=""><Calendar2CheckFill/>DATA</p>
+                    <p className="events-participants"><PeopleFill/>QUANTIDADE DE PARTICIPANTES</p>
+                    <p className=""><ChatSquareTextFill/>DESCRICAO</p>
+                </div>
+
+            </div>
         </div>
-        /* 
 
-        <h1>EVENTO</h1>
-        <p className="event-city">CIDADE DO EVENTO</p>
-        <p className="event-owner">DONO DO EVENTO</p>
-        <p className="">DATA</p>
-        <p className="events-participants">QUANTIDADE DE PARTICIPANTES</p>
-        <p className="">DESCRICAO</p>
-
-        */
     );
 }
 
