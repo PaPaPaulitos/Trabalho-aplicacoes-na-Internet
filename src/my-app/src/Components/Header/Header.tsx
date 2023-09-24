8b935f63b25c187fdfb790a1eba1d67954cfc2e1
Paulo Ricardo Mesquita <87778216+PaPaPaulitos@users.noreply.github.com>
2023-09-24 14:13:50 -0300

    Adicionando a logo

--- src/my-app/src/Components/Header/Header.tsx
+++ src/my-app/src/Components/Header/Header.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './Header.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
-
+import img  from '../../img/logo.png';
 
 function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
-            <Link className="navbar-brand" to='/' >LOGO</Link>
+            <img src={ img } ></img>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
