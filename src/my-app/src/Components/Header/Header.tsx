f4aa98ce75d45e0efe4ed137b903d484b520645a
Paulo Ricardo Mesquita <87778216+PaPaPaulitos@users.noreply.github.com>
2023-09-26 11:35:18 -0300

    Fazendo o menu hamburguer funcionar

--- src/my-app/src/Components/Header/Header.tsx
+++ src/my-app/src/Components/Header/Header.tsx
@@ -3,22 +3,29 @@ import './Header.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import img  from '../../img/logo.png';
+import { useState } from 'react';
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light">
-            <img src={ img } className='logo-eve'></img>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <img src={img} className='logo-eve' alt="Logo" />
+            <button className="navbar-toggler" type="button" onClick={toggleMenu} style={{ zIndex: isOpen ? 1 : 0 }}>
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="listaDeLinks" style={{ zIndex: isOpen ? 1 : 0 }}>
                 <div className="navbar-nav">
-                            <Link className="nav-item nav-link" to='/' >Eventos</Link>
-                            <Link className="nav-item nav-link" to='createevent' >Criar Evento</Link>
-                            <Link className=" nav-item nav-link" to='/login' >Entrar</Link>
+                    <Link className={`nav-item nav-link ${isOpen ? 'menu-layer' : ''}`} to="/">Eventos</Link>
+                    <Link className={`nav-item nav-link ${isOpen ? 'menu-layer' : ''}`} to="createevent">Criar Evento</Link>
+                    <Link className={`nav-item nav-link ${isOpen ? 'menu-layer' : ''}`} to="/login">Entrar</Link>
                 </div>
             </div>
-    </nav>
+        </nav>
     );
 }
 
